3a3e9aec4a36eeb48b1d7e6398aa17bd32d77c8a
Baris12303 <baaarris003@gmail.com>
2026-05-15 17:15:17 +0300

    Add WhatsAppServisi class for sending messages

---
+++ src/WhatsAppServisi.java
@@ -0,0 +1,6 @@
+public class WhatsAppServisi {
+    public void sendMessage(String phone, String text) {
+        System.out.println("WhatsApp API'ye bağlanılıyor...");
+        System.out.println(phone + " nolu telefona WP mesajı gitti: " + text);
+    }
+}
