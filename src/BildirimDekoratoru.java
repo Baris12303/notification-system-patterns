6396e0eabdbd9dbbaaa0429be3610d311786832b
Baris12303 <baaarris003@gmail.com>
2026-05-15 17:27:00 +0300

    Create BildirimDekoratoru.java

---
+++ src/BildirimDekoratoru.java
@@ -0,0 +1,12 @@
+public abstract class BildirimDekoratoru implements Bildirim {
+    protected Bildirim sarilanBildirim;
+
+    public BildirimDekoratoru(Bildirim bildirim) {
+        this.sarilanBildirim = bildirim;
+    }
+
+    @Override
+    public void gonder(String mesaj, String hedef) {
+        sarilanBildirim.gonder(mesaj, hedef);
+    }
+}
