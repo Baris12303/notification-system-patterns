ef5a87bcaef8ae4f1e060cea0fd3bd4d19fa8d3d
Baris12303 <baaarris003@gmail.com>
2026-05-21 11:00:40 +0300

    Update BildirimYoneticisi.java

--- src/bildirim/BildirimYoneticisi.java
+++ src/bildirim/BildirimYoneticisi.java
@@ -1,19 +1,19 @@
 package bildirim;
 
-public class BildirimYonetici {
+// BildirimYoneticisi: Fabrika'yi kullanarak bildirim gonderme isini organize eder.
+// Bu sinif dogrudan nesne uretmiyor; uretimi BildirimFabrikasi'na devrediyor (SRP).
+public class BildirimYoneticisi {
     
     private BildirimFabrikasi fabrika;
 
-    public BildirimYonetici() {
+    public BildirimYoneticisi() {
         this.fabrika = new BildirimFabrikasi();
     }
 
     public void bildirimGonder(String tip, String mesaj, String hedef) {
         try {
             Bildirim bildirim = fabrika.bildirimOlustur(tip);
-            
             bildirim.gonder(mesaj, hedef);
-            
         } catch (IllegalArgumentException e) {
             System.out.println("Hata: " + e.getMessage());
         }
